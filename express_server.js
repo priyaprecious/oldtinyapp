75dcb93d50f14c4f5778369603358cd5c6a5691d
priyaprecious <priyanka.datti@gmail.com>
2021-11-09 01:19:08 +0000

    updated ejs

--- express_server.js
+++ express_server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */req.params.longURL };
+    res.render("urls_show", templateVars);
+  });
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
